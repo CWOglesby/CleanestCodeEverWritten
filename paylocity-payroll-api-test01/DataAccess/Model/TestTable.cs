e8e0b3401c8b77770eefcb4f0af1de163cc5e4fb
Chris Oglesby <cwoglesby@gmail.com>
2023-03-05 21:04:42 -0600

    Scaffolded PayrollDataContext from PayrollDatabase.

---
+++ paylocity-payroll-api-test01/DataAccess/Model/TestTable.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace paylocity_payroll_api_test01.DataAccess.Model;
+
+public partial class TestTable
+{
+    public long TestId { get; set; }
+}
