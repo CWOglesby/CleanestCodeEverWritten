c172803f51e7ffd25dd061bde4e0542d81619f07
Chris Oglesby <cwoglesby@gmail.com>
2023-03-06 00:56:23 -0600

    More cleanup before DB schema script.

--- paylocity-payroll-api-test01/Program.cs
+++ paylocity-payroll-api-test01/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using paylocity_payroll_api_test01.DataAccess;
 
 namespace paylocity_payroll_api_test01
 {
@@ -10,9 +9,9 @@ namespace paylocity_payroll_api_test01
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var payrollDbConnection = builder.Configuration["ConnectionStrings:PayrollDatabase"];
-            builder.Services.AddDbContext<PayrollDbContext>(options =>
-                options.UseSqlServer(payrollDbConnection));
+            //var payrollDbConnection = builder.Configuration["ConnectionStrings:PayrollDatabase"];
+            //builder.Services.AddDbContext<PayrollDbContext>(options =>
+            //    options.UseSqlServer(payrollDbConnection));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
